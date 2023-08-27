46c0eafa52d84386f5b56a5d74d2c09b6f1eb4f1
Earl Hans Genoso <earlhansg@gmail.com>
2023-08-28 06:28:33 +0800

    Client - added header user interface in contact component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './contact/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
