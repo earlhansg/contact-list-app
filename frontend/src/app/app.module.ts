dfb30019853f54eb80fecb8297c0beb43e41c9cf
Earl Hans Genoso <earlhansg@gmail.com>
2023-08-28 07:12:59 +0800

    Client - added filter ui component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './contact/contact.component';
 import { HeaderComponent } from './contact/header/header.component';
+import { FilterComponent } from './contact/filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    HeaderComponent
+    HeaderComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
